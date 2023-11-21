527e7aff40ea45d592a61142ece8121aef828a53
Javier Frias <96632596+spuk0@users.noreply.github.com>
2023-11-21 00:10:54 -0300

    Interbloqueo por concurrencia de transacciones

---
+++ Interbloqueo por concurrencia de transacciones/Script1.sql
@@ -0,0 +1,37 @@
+/*
+Abrir este y el otro script que se encuentran en la misma carpeta, cada uno en una ventana de consulta distinta
+en el DBMS (SQL Server Management Studio en nuestro caso).
+La explicacion se encuentra comentada en cada script.
+Para observar el interbloqueo, ejecutar *PRIMERO* este script, ambos hacen un update pero este cuenta con un delay
+para dar tiempo a ejecutar el otro y poder simular la concurrencia de transacciones
+*/
+USE base_consorcio_proyecto;
+GO
+
+--TRANSACCION UNO
+
+--PASO 1
+
+--INICIAR TRANSACCION
+BEGIN TRAN
+	UPDATE conserje SET apeynom  = 'Update 1 Transaccion 1'
+	WHERE idconserje = 1
+	WAITFOR DELAY '00:00:10' --TIEMPO DE ESPERA PARA EJECUTAR EL OTRO SCRIPT
+
+--PASO 3
+	UPDATE administrador SET apeynom = 'Update 2 Transaccion 1'
+	WHERE idadmin = 1
+
+--CONFIRMAR TRANSACCION
+COMMIT TRAN
+
+--Por ultimo se hace una consulta para mostrar cual actualizacion fue la que se realizo
+SELECT * FROM conserje
+SELECT * FROM administrador
+
+/*
+Deadlock, Punto muerto o Interbloqueo.
+En una ventana se actualizaran los resultados y en la otra se mostrara un error de interbloqueo ya que la
+transaccion que actualizo los resultados tiene bloqueado el proceso y la transaccion de la otra ventana no 
+puede acceder a el.
+*/
